198ea67491980ada16f54da71fa97006662589df
MrGSommer <110161907+MrGSommer@users.noreply.github.com>
2024-10-16 13:37:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ def login():
     Zeigt das Login-Formular an und überprüft den eingegebenen Benutzernamen und das Passwort.
     Wenn die Anmeldedaten korrekt sind, wird der Benutzer als eingeloggt markiert.
     """
-    st.set_page_config(layout="wide")  # Aktiviert den "Wide Mode"
+
     st.session_state["logged_in"] = False
     
     st.write("Bitte einloggen")
@@ -232,6 +232,7 @@ def app():
     Stellt sicher, dass der Benutzer eingeloggt ist, bevor die Hauptanwendung gestartet wird.
     Wenn der Benutzer nicht eingeloggt ist, wird das Login-Formular angezeigt.
     """
+    st.set_page_config(layout="wide")  # Aktiviert den "Wide Mode"
     if "logged_in" not in st.session_state or not st.session_state["logged_in"]:
         login()
     else:
